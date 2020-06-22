226b950fdaf69ce77b88c698317dac5dd126e083
SivenM <aztek8877@gmail.com>
2020-06-22 16:15:42 +0300

    OutBlock added

--- layers.py
+++ layers.py
@@ -5,6 +5,7 @@ class UpSampleBlock(tf.keras.layers.Layer):
     """Conv2DTranspose => Batchnorm => Dropout => Relu"""
     def __init__(self, filters, size, apply_dropout=False):
         super(UpSampleBlock, self).__init__()
+        self.apply_dropout = apply_dropout
         self.initializer = tf.random_normal_initializer(0., 0.02)
         self.conv_transpose = tf.keras.layers.Conv2DTranspose(
                                       filters, size, strides=2,
@@ -19,5 +20,24 @@ class UpSampleBlock(tf.keras.layers.Layer):
     def call(self, inputs):
         x = self.conv_transpose(inputs)
         x = self.batch_norm(x)
+        if self.apply_dropout:
+            x = self.dropout(x)
         return self.relu(x)
 
+
+class OutBlock(tf.keras.layers.Layer):
+    """Conv2DTranspose => Conv2D => softmax"""
+    def __init__(self, filters, size, num_classes=2):
+        super(OutBlock, self).__init__()
+        self.initializer = tf.random_normal_initializer(0., 0.02)
+        self.conv_transpose = tf.keras.layers.Conv2DTranspose(
+                                      filters, size, strides=2,
+                                      padding='same',
+                                      kernel_initializer=self.initializer,
+                                      use_bias=False
+                                      )
+        self.conv2D = tf.keras.layers.Conv2D(num_classes, 1, activation='softmax')
+
+    def call(self, inputs):
+        x = self.conv_transpose(inputs)
+        return self.conv2D(x)
