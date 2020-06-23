b3fb93f3f81b16b24f785a8032815759f0135c43
SivenM <aztek8877@gmail.com>
2020-06-23 11:59:56 +0300

    add metrics: DICE and IOU

---
+++ metrics.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+from tensorflow.keras import backend as K
+import numpy as np
+
+
+# Metrics
+def dice_coef(y_true, y_pred, smooth=1):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+
+def iou(y_true, y_pred, smooth=1):
+    intersection = K.sum(K.abs(y_true * y_pred), axis=-1)
+    union = K.sum(y_true, -1) + K.sum(y_pred, -1) - intersection
+    result = (intersection + smooth) / (union + smooth)
+    return result
+
+
