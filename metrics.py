f7a55040d23cb7644921a2a7ae493b9ce3aab0a1
SivenM <aztek8877@gmail.com>
2020-06-24 10:35:27 +0300

    dice without bg

--- metrics.py
+++ metrics.py
@@ -3,8 +3,8 @@ from tensorflow.keras import backend as K
 
 # Metrics
 def dice_coef(y_true, y_pred, smooth=1):
-    y_true_f = K.flatten(y_true)
-    y_pred_f = K.flatten(y_pred)
+    y_true_f = K.flatten(y_true[:,:,:,1])
+    y_pred_f = K.flatten(y_pred[:,:,:,1])
     intersection = K.sum(y_true_f * y_pred_f)
     return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
 
